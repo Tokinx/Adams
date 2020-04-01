eab898d5a888cf3b64ecde1c74422cc3257b7c7f
Frank <zhwuzhuo@gmail.com>
2020-04-01 10:17:57 +0800

    is_robots应该是个方法，而不是常量

--- functions.php
+++ functions.php
@@ -164,7 +164,7 @@ add_filter("mce_buttons_3", "enable_more_buttons");
 // 代码高亮
 function dangopress_esc_html($content)
 {
-    if (!is_feed() || !is_robots) {
+    if (!is_feed() || !is_robots()) {
         $content = preg_replace('/<code(.*)>/i', "<code class=\"prettyprint\" \$1>", $content);
     }
     $regex = '/(<code.*?>)(.*?)(<\/code>)/sim';
