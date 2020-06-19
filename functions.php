5c76093ef9df5801c956bdee7484c41c3289ce22
Cheng <idevs@foxmail.com>
2020-06-19 10:46:55 +0800

    修复文章结尾tags标签过多无法自动换行的问题

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @subpackage Adams
  */
 if (!defined('THEME_NAME')) define('THEME_NAME', 'Adams');
-if (!defined('THEME_DB_VERSION')) define('THEME_DB_VERSION', 'v1.4.28');
+if (!defined('THEME_DB_VERSION')) define('THEME_DB_VERSION', 'v1.4.29   ');
 if (version_compare($GLOBALS['wp_version'], '4.4-alpha', '<')) {
     wp_die('Please upgrade to version 4.4 or higher');
 }
