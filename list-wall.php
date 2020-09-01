0daf1ff60408b4b90e5b22f4f5b9d8463fc9111c
Cheng <idevs@foxmail.com>
2020-09-01 09:11:45 +0800

    fix ListWall

--- list-wall.php
+++ list-wall.php
@@ -7,10 +7,10 @@ get_header(); ?>
         <article class="post_article readerswall" itemscope itemtype="https://schema.org/Article">
             <ul>
                 <?php
-                $query = "SELECT COUNT(comment_ID) AS cnt, comment_author, comment_author_url, comment_author_email FROM (SELECT * FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->posts.ID=$wpdb->comments.comment_post_ID) WHERE comment_date > date_sub( NOW(), INTERVAL 2 MONTH ) AND user_id='0' AND post_password='' AND comment_approved='1' AND comment_type='') AS tempcmt GROUP BY comment_author_email ORDER BY cnt DESC LIMIT 24";
+                $query = "SELECT COUNT(comment_ID) AS cnt, comment_author, comment_author_url, comment_author_email FROM (SELECT * FROM $wpdb->comments LEFT OUTER JOIN $wpdb->posts ON ($wpdb->posts.ID=$wpdb->comments.comment_post_ID) WHERE comment_date > date_sub( NOW(), INTERVAL 3 MONTH ) AND user_id='0' AND post_password='' AND comment_approved='1' AND comment_type='comment') AS tempcmt GROUP BY comment_author_email ORDER BY cnt DESC LIMIT 12";
                 foreach ($wpdb->get_results($query) as $comment) { ?>
                     <li>
-                        <a href="<?= ($comment->comment_author_url ?: 'javascript:void(0);') ?>" target="_blank"
+                        <a href="<?= ($comment->comment_author_url ?: 'javascript:void(0);') ?>" target="<?= ($comment->comment_author_url ? '_blank' : '_self') ?>"
                            title="<?= $comment->comment_author ?>（<?= $comment->cnt ?>）">
                             <?= get_avatar($comment->comment_author_email, 160); ?>
                             <span><?= $comment->comment_author ?><br></span>
