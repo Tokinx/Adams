e7df4d13f944ae0672fae95b6aeddfb65eccb432
cc2007 <88186750+cc2007@users.noreply.github.com>
2023-10-15 01:45:36 +0800

    Update functions.php
    更新阅读数量计算

--- functions.php
+++ functions.php
@@ -590,3 +590,41 @@ $mytheme_update_checker = new ThemeUpdateChecker(
 );
 
 // 全部配置完毕
+
+//阅读数量计算  
+function get_post_views ($post_id) {   
+  
+    $count_key = 'views';   
+    $count = get_post_meta($post_id, $count_key, true);   
+  
+    if ($count == '') {   
+        delete_post_meta($post_id, $count_key);   
+        add_post_meta($post_id, $count_key, '0');   
+        $count = '0';   
+    }   
+  
+    echo number_format_i18n($count);   
+  
+}   
+  
+function set_post_views () {   
+  
+    global $post;   
+  
+    $post_id = $post -> ID;   
+    $count_key = 'views';   
+    $count = get_post_meta($post_id, $count_key, true);   
+  
+    if (is_single() || is_page()) {   
+  
+        if ($count == '') {   
+            delete_post_meta($post_id, $count_key);   
+            add_post_meta($post_id, $count_key, '0');   
+        } else {   
+            update_post_meta($post_id, $count_key, $count + 1);   
+        }   
+  
+    }   
+  
+}   
+add_action('get_header', 'set_post_views');  
