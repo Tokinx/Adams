01b18703ea7e79768abb325ff68a2f10292de8c5
cc2007 <88186750+cc2007@users.noreply.github.com>
2023-10-17 22:45:28 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -628,3 +628,13 @@ function set_post_views () {
   
 }   
 add_action('get_header', 'set_post_views');  
+
+//字数统计
+function count_words ($text) {
+global $post;
+if ( '' == $text ) {
+   $text = $post->post_content;
+   if (mb_strlen($output, 'UTF-8') < mb_strlen($text, 'UTF-8')) $output .= '本文共' . mb_strlen(preg_replace('/\s/','',html_entity_decode(strip_tags($post->post_content))),'UTF-8') . '个字';
+   return $output;
+}
+}
